17391a3d3bbe01f42f828a20a12579fc50eec4d0
weslei sacal <wsacal@gmail.com>
2018-11-28 22:38:29 -0300

    Atualizando

---
+++ Switch.Infra.Data/Config/PostagemConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Switch.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Infra.Data.Config
+{
+    public class PostagemConfiguration : IEntityTypeConfiguration<Postagem>
+    {
+
+        public void Configure(EntityTypeBuilder<Postagem> builder)
+        {
+            builder.HasKey(u => u.Id);
+            builder.Property(p => p.DataPublicacao).IsRequired();
+            builder.Property(p => p.Texto).IsRequired().HasMaxLength(400);
+            builder.HasOne(p => p.Usuario).WithMany(u => u.Postagens);
+
+
+        }
+    }
+}
--- Switch.Infra.Data/Config/UsuarioConfiguration.cs
+++ Switch.Infra.Data/Config/UsuarioConfiguration.cs
@@ -21,7 +21,8 @@ namespace Switch.Infra.Data.Config
             builder.Property(u => u.DataNascimento).HasMaxLength(400).IsRequired();            
             builder.HasOne(u => u.Identificacao)
                 .WithOne(i => i.Usuario)
-                .HasForeignKey<Identificacao>(i => i.UsuarioId);    
+                .HasForeignKey<Identificacao>(i => i.UsuarioId);
+            builder.HasMany(u => u.Postagens).WithOne(p => p.Usuario);
         }
     }
 }
